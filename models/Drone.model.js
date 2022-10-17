ac8987c7569fc504a2a6495f1d0c8edbf72ad8b6
Borja Cabello Luna <borjacabello@outlook.es>
2022-10-17 17:12:11 +0200

    iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+const mongoose = require("mongoose")
+
+// Schema
+const droneSchema = new mongoose.Schema({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+// Model
+const Drone = mongoose.model("Drone", droneSchema);
+
+// Export
+module.exports = Drone
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,34 @@
 // Iteration #1
+const mongoose = require("mongoose")
+require("../db")
+const Drone = require("../models/Drone.model")
+
+// Data
+const drones = [
+    {
+        name: "pokemons",
+        propellers: 5,
+        maxSpeed: 24
+    },
+    {
+        name: "patata",
+        propellers: 2,
+        maxSpeed: 10
+    },
+    {
+        name: "drone2000",
+        propellers: 8,
+        maxSpeed: 57
+    },
+]
+
+// Add arrays to the database
+Drone
+.create(drones)
+.then((response) => {
+    console.log(response.length);
+    mongoose.connection.close();
+})
+.catch((err) => {
+    console.log(err)
+})
